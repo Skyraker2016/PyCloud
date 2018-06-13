55408afebd89e63cbfca6d9a622d418bb7eee8b2
Easyin <304005737@qq.com>
2018-06-13 23:06:55 +0800

    craw baike

--- crawler.py
+++ crawler.py
@@ -8,11 +8,15 @@ def getBeautifulSoupObj(name):
     suffix = urllib.parse.quote(name) # 获取后缀
     html = urlopen("https://baike.baidu.com/item/" + suffix)
     bsObj = BeautifulSoup(html, "html5lib")
+    if bsObj.find("title").get_text() == "百度百科——全球最大中文百科全书":
+        return None
     return bsObj
 
 def getBaikeText(name):
     '''接收中文姓名，返回对应百度百科的文字信息，若无则返回0'''
     bsObj = getBeautifulSoupObj(name)
+    if bsObj == None:
+        return None
     paragraph = bsObj.findAll("div", {"class": "para"})
     # paragraph = bsObj.findAll("div", text = "para")
     text = ""
@@ -23,12 +27,13 @@ def getBaikeText(name):
     f.close()
     return text
 
-def getBaikePhoto(name):
+def getgBaikePhoto(name):
     '''接收中文姓名，返回对应百度百科的第一张图片'''
     bsObj = getBeautifulSoupObj(name)
+    if bsObj == None:
+        return None
     try:
         photourl = str(bsObj.find("div", {"class": "summary-pic"}).find("img")).split("\"")[1]
     except AttributeError:
         return None
     urlretrieve(photourl, name + ".jpg")
-
